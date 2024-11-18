b067b2968b2006e8bf8f376602637e09e82c5994
Toby Harris <51968582+tobyaharris@users.noreply.github.com>
2024-11-18 19:29:32 +0000

    Added descriptive messages to events
    I would rather were in a separate Analysis module
    that observed the MQTT message and sent another,
    but here is working with the tools at hand.
    At least it is a short snippet.

--- UserConfig/Sensing/main.py
+++ UserConfig/Sensing/main.py
@@ -1,7 +1,7 @@
 """main.py that prepares the Sensing service module for Count Monitoring.
 
-All configuration is done in the Settings section
-Assumes sensors have external pulls
+All configuration is done in the Settings section.
+Assumes sensors have pulls external to the GPIO interface.
 """
 
 ## -- Imports ---------------------------------------------------------------------
@@ -37,6 +37,23 @@ count_channels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 # Wiring polarity
 active_state = 0
 
+# Describe events by the number of sensors that were active when the trigger sensor was held
+events = {
+    1: "Undefined",
+    2: "Undefined",
+    3: "Fancoil_600",
+    4: "Fancoil_600",
+    5: "Fancoil_900",
+    6: "Fancoil_900",
+    7: "Fancoil_1200",
+    8: "Fancoil_1200",
+    9: "Fancoil_1500",
+    10: "Fancoil_1500",
+    11: "Fancoil_1800",
+    12: "Fancoil_1800",
+    13: "Fancoil_2050",
+}
+
 ## --------------------------------------------------------------------------------
 
 
@@ -52,9 +69,11 @@ while True:
 
         if time.time() > (start_time + trigger_hold_time):
 
+            buttons_pressed = input_interface.read_multiple_channels(count_channels).count(active_state)
             publish( {
-                "machine": machine_name,
-                "buttons_pressed" : input_interface.read_multiple_channels(count_channels).count(active_state),
+                "machine"         : machine_name,
+                "buttons_pressed" : buttons_pressed,
+                "event"           : events[buttons_pressed],
                 } )
 
             while input_interface.read_single_channel(trigger_channel) == active_state:
